b409465b94ceca4a04be9b13a4c31247718fecae
Arina <arina.milyutina.0005@gmail.com>
2022-11-26 02:35:41 +0300

    commit1

---
+++ src/Lesson3_2.java
@@ -0,0 +1,11 @@
+public class Lesson3_2 {
+    public static void main(String[] args) {
+        Number();
+    }
+
+    public static void Number() {
+        for (int i = 5; i > 0; i--) {
+            System.out.println(i);
+        }
+    }
+}
