22bddb85f88fa0b5c66617b835f44c3071d10491
Arina <arina.milyutina.0005@gmail.com>
2022-11-26 00:37:04 +0300

    commit1

---
+++ src/Lesson3_2.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class Lesson3_2 {
+public static void main(String[]args){
+    TimeOfYear();
+
+}
+public static void TimeOfYear(){
+System.out.println("Enter the number month,please.");
+    Scanner scanner=new Scanner(System.in);
+    int number= scanner.nextInt();
+    if(number==1||number==2||number==12){
+        System.out.println("Winter!");
+    }
+    else if(number==3||number==4||number==5){
+        System.out.println("Spring!");
+    }
+    else if(number==6||number==7||number==8){
+        System.out.println("Summer!");
+    }
+    else if(number==9||number==10||number==11){
+        System.out.println("Autumn!");
+    }
+    System.out.println("\nEnd of the method.");
+
+}
+}
