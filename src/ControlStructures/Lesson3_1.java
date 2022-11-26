1b7aef94953e2be946e2da630860e6a3e8c082a0
Arina <arina.milyutina.0005@gmail.com>
2022-11-26 05:41:26 +0300

    commit1

--- src/ControlStructures/Lesson3_1.java
+++ src/ControlStructures/Lesson3_1.java
@@ -1,9 +1,9 @@
+
 package ControlStructures;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Scanner;
-
 public class Lesson3_1 {
     public static void main(String[] args) throws IOException {
         int number = 0;
