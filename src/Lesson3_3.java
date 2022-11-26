2a9092b71be93b920106565e6fd083bd94f534c4
Arina <arina.milyutina.0005@gmail.com>
2022-11-26 03:30:57 +0300

    commit1

---
+++ src/Lesson3_3.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Lesson3_3 {
+    public static void main(String[] args) {
+        SumOfNumbers();
+    }
+
+    public static void SumOfNumbers() {
+        int sum = 0;
+        int c;
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter the number,please.To end the method,enter the number 0.");
+        int number = scanner.nextInt();
+        c = number % 2;
+            for (int i = 0; i <= number; i++) {
+                sum += i;
+            }
+            if (c == 0) {
+                System.out.println("Sum of numbers:" + sum);
+            } else System.out.println("This number is uneven:");
+        }
+    }
