db379c214f0b31cdf4275e492bc914cba24e8571
Arina <arina.milyutina.0005@gmail.com>
2022-11-26 05:15:22 +0300

    commit1

--- src/Lesson3_2.java
+++ src/Lesson3_2.java
@@ -1,11 +1,18 @@
-public class Lesson3_2 {
-    public static void main(String[] args) {
-        Number();
-    }
+import java.util.Scanner;
 
-    public static void Number() {
-        for (int i = 5; i > 0; i--) {
-            System.out.println(i);
+public class Lesson3_2 {
+
+    public static void main(String[] args) {
+
+        Scanner in = new Scanner(System.in);
+        System.out.print("Enter the amount of money:");
+        float money = in.nextFloat();
+        System.out.print("Enter the period:");
+        int period = in.nextInt();
+
+        for(int i = 1; i <= period; i++){
+            money += money * 0.07;
         }
+        System.out.printf("After %d month the deposit amount will be %f", period, money);
     }
 }
