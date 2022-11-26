e183f98c97112d73f1802e33a14c06dddd2a4cde
Arina <arina.milyutina.0005@gmail.com>
2022-11-26 04:57:17 +0300

    commit1

--- src/Lesson3_1.java
+++ src/Lesson3_1.java
@@ -1,11 +1,12 @@
-public class Lesson3_1 {
-    public static void main(String[]args) {
-        EvenNumbers();
-    }
-    public static void EvenNumbers(){
-        for (int i=1; i<100; i++)
-        {
-            if (i%2 !=0)
-                System.out.print(i + " " );
+public class Lesson3_1{
+    public static void main(String[] args){
+        int a=1,b=1,c=0;
+        System.out.print(a+" "+b+" ");
+        for(int i=3;i<=11;i++){
+            c=a+b;
+            a=b;
+            b=c;
+            System.out.print(c+" ");
         }
-    }}
+    }
+}
