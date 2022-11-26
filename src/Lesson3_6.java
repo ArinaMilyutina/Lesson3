e3130360355cc43b7cefa1962c21a2068fc6c8f3
Arina <arina.milyutina.0005@gmail.com>
2022-11-26 04:23:48 +0300

    commit1

---
+++ src/Lesson3_6.java
@@ -0,0 +1,12 @@
+public class Lesson3_6 {
+    public static void main(String[]args){
+        SquaresOfNumbers();
+    }
+    public static void SquaresOfNumbers(){
+         int a=2;
+        for(int i=10;i<21;i++){
+            System.out.println(i+"^2="+Math.pow(i,a)+";");
+
+        }
+    }
+}
