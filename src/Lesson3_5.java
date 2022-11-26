fd4f1151281c05a04e069218df94faf33da4be9f
Arina <arina.milyutina.0005@gmail.com>
2022-11-26 04:10:48 +0300

    commit1

---
+++ src/Lesson3_5.java
@@ -0,0 +1,16 @@
+public class Lesson3_5 {
+    public static void main(String[] args) {
+        Numbers();
+    }
+
+    public static void Numbers() {
+        int sum=0;
+        int count=0;
+        for (int i=sum;i<10;i++){
+          count++;
+          System.out.println(count+" sequence element="+sum);
+          sum+=-5;
+        }
+
+    }
+}
