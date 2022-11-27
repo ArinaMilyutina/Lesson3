bb8de42922d0cb7207d28f83bd9da498602263e6
Arina <arina.milyutina.0005@gmail.com>
2022-11-27 21:54:06 +0300

    commit2

--- src/Cycle/Lesson3_3.java
+++ src/Cycle/Lesson3_3.java
@@ -8,17 +8,14 @@ public class Lesson3_3 {
 
     public static void SumOfNumbers() {
         int sum = 0;
-        int c;
-
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Enter the number,please.To end the method,enter the number 0.");
+        System.out.println("Enter the number,please.");
         int number = scanner.nextInt();
-        c = number % 2;
             for (int i = 0; i <= number; i++) {
                 sum += i;
             }
-            if (c == 0) {
+            if (number>0) {
                 System.out.println("Sum of numbers:" + sum);
-            } else System.out.println("This number is uneven:");
+            } else System.out.println("This number is negative.");
         }
     }
